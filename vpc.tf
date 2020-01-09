9ec7e90ce44b6643a22d94b692681dc44e79a8bf
Vihar Patel <patel486@purdue.edu>
2020-01-09 23:58:30 +0000

    Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,10 @@
+
+module "vpc" {
+  source  = "app.terraform.io/tf-learn-vihar/vpc/aws"
+  version = "2.21.0"
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "dedicated"
