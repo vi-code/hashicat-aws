b7b10612a065f97cbf162d0459c63c6a3fdb2601
Vihar Patel <patel486@purdue.edu>
2020-01-10 00:02:30 +0000

    yolo

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,20 @@
-
 module "vpc" {
-  source  = "app.terraform.io/tf-learn-vihar/vpc/aws"
-  version = "2.21.0"
-}
+  source = "terraform-aws-modules/vpc/aws"
 
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "dedicated"
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    Name = "main"
-    Billing = "true"
+    Terraform = "true"
+    Environment = "dev"
+    Billable = "true"
     Department = "devops"
   }
 }
